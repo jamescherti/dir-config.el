ae82852373e7a60a2b1ab71266d7b808d2c37b64
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-08-15 23:18:12 -0400

    Add continuous integration files: Cask, Makefile, GitHub workflowh

---
+++ tests/test-local-emacs-rc.el
@@ -0,0 +1,36 @@
+;;; test-local-emacs-rc.el --- Short description of your project  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2024 James Cherti | https://www.jamescherti.com/contact/
+
+;; Author: James Cherti
+;; Version: 0.9.9
+;; URL: https://github.com/jamescherti/local-emacs-rc.el
+;; Keywords: convenience
+;; Package-Requires: ((emacs "24.1"))
+;; SPDX-License-Identifier: GPL-3.0-or-later
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 2, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;; Test local-emacs-rc.
+
+;;; Code:
+
+(defun test-local-emacs-rc ()
+  "Test local-emacs-rc."
+  (interactive)
+  t)
+
+(provide 'test-local-emacs-rc)
+;;; test-local-emacs-rc.el ends here

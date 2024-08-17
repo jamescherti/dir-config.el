5cbc2655a5ee734a4747a1b580e331e88c720737
James Cherti <60946298+jamescherti@users.noreply.github.com>
2024-08-17 12:07:13 -0400

    Release: 1.0.1

--- dir-config.el
+++ dir-config.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2003-2024  James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; URL: https://github.com/jamescherti/dir-config.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "25.1"))
--- tests/test-dir-config.el
+++ tests/test-dir-config.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2023-2024 James Cherti | https://www.jamescherti.com/contact/
 
 ;; Author: James Cherti
-;; Version: 1.0.0
+;; Version: 1.0.1
 ;; URL: https://github.com/jamescherti/dir-config.el
 ;; Keywords: convenience
 ;; Package-Requires: ((emacs "24.1"))
